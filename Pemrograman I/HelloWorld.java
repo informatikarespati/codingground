588d72f5ef1449b684e6be2cca9442474c1c0a49
informatikarespati <31459319+informatikarespati@users.noreply.github.com>
2017-08-30 07:42:41 +0700

    Created by Coding Ground Client

---
+++ Pemrograman I/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Nama : Anggun Sihombing");
+        System.out.println("NIM : 15220001");
+     }
+}
