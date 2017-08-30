1e80ecdda301c23ce11cd64e6a91c0f3c54defa3
informatikarespati <31459319+informatikarespati@users.noreply.github.com>
2017-08-30 08:33:50 +0700

    Created by Coding Ground Client

---
+++ Pemrograman I/Belajar01.java
@@ -0,0 +1,11 @@
+public class Belajar01 {
+    public static void main(String[] args) {
+        double berat;
+        berat = 41.2;
+        System.out.println("Berat Badan = " + berat);
+        berat = berat + 2;
+        System.out.println("Berat Badan = " + berat);
+        berat = berat - 1;
+        System.out.println("Berat Badan = " + berat);
+    }
+}
