b50dfc4fee97800fa3c698c46888413f94cbff47
informatikarespati <31459319+informatikarespati@users.noreply.github.com>
2017-08-30 08:32:19 +0700

    Created by Coding Ground Client

---
+++ Pemrograman I/Belajar02.java
@@ -0,0 +1,17 @@
+public class Belajar02 {
+    public static void main(String[] args) {
+        int panjang;
+        int lebar;
+        int luas;
+        int keliling;
+        
+        panjang = 10;
+        lebar = 15;
+        
+        luas = panjang * lebar;
+        keliling = 2 * (panjang + lebar);
+        
+        System.out.println("Luas = " + luas);
+        System.out.println("Keliling = " + keliling);
+    }
+}
